35850b9d285b8bc004a117c5ee06449091693edd
SungyoonRyu <rara0620@naver.com>
2024-08-06 16:29:34 +0900

    feat: add status and show status route
    get raw data of status

--- ML/ML-Server/router/board.py
+++ ML/ML-Server/router/board.py
@@ -1,31 +1,43 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from typing import Optional
 
 from service import *
 from utils_main import *
+import main
 
 TRIAL = 4
 router = APIRouter(
     prefix='/api/board'
 )
 
-@router.get('game-start')
-async def game_start():
-    pass
+@router.post('game-start')
+async def game_start(player_num: int):
+    main.app.game.game_start(player_num)
 
 # -----------------------------------------------------------
 # 객체 클러스터링으로 데이터 정제 후, CC에 JSON 형태로 전송
 # 작업 중
 
 
-@router.get('/game-status')
-async def get_game_status():
-    
+@router.get('/raw-status')
+async def get_status_raw():
+    return JSONResponse(main.app.game.__dict__)
+
+
+@router.post('/game-status')
+async def get_game_status(situate: Optional[dict]):
+    # TODO: return error message
+    if not main.app.game.running:
+        return
+
     for tried in range(TRIAL):
         print("inference try... ", tried)
         results = await convert_to_dict(inference("dir"))
         result = tracePlayers(results)
         if result is not None:
+            main.app.game.playersCard = result[0]
+            main.app.game.deckCard = result[1]
             result = {
                 "user_card": result[0],
                 "deck_card": result[1]
