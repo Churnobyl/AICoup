1929563fd65afdc6d549a5483821761040b4f8c8
성철민 <tjdcjfals3@naver.com>
2024-08-12 16:35:53 +0900

    FE/ history memoization

--- CC/FE/src/components/ui/sheets/HistoryBottomSheet.tsx
+++ CC/FE/src/components/ui/sheets/HistoryBottomSheet.tsx
@@ -1,21 +1,35 @@
 import useGameStore from "@/stores/gameStore";
-import { useEffect, useRef, useState } from "react";
+import { memo, useEffect, useMemo, useRef, useState } from "react";
 import { BottomSheet, BottomSheetRef } from "react-spring-bottom-sheet";
 import "react-spring-bottom-sheet/dist/style.css";
 import "./HistoryBottomSheet.scss";
 import useHistoryStore from "@/stores/historyMessageStore";
 
-function HistoryBottomSheet() {
+const HistoryBottomSheet = memo(() => {
   const sheetRef = useRef<BottomSheetRef>(null);
   const lastItemRef = useRef<HTMLLIElement>(null);
   const [open, setOpen] = useState(true);
   const store = useGameStore();
   const { history } = store;
-  const { historyMessage } = useHistoryStore();
+  const { historyMessage } = useHistoryStore((state) => ({
+    historyMessage: state.historyMessage,
+  }));
 
   const handleDismiss = () => {
     setOpen(false);
   };
+
+  const renderedHistory = useMemo(() => {
+    return historyMessage.map((message, index) => (
+      <li
+        key={index}
+        ref={index === historyMessage.length - 1 ? lastItemRef : null}
+        className={index % 2 === 0 ? "gray" : ""}
+      >
+        {message}
+      </li>
+    ));
+  }, [historyMessage]);
 
   useEffect(() => {
     if (lastItemRef.current) {
@@ -35,20 +49,10 @@ function HistoryBottomSheet() {
         defaultSnap={({ maxHeight }) => maxHeight / 4}
         expandOnContentDrag={true}
       >
-        <ul className="history-list">
-          {historyMessage.map((value, index) => (
-            <li
-              key={index}
-              ref={index === historyMessage.length - 1 ? lastItemRef : null}
-              className={index % 2 === 0 ? "gray" : ""}
-            >
-              {value}
-            </li>
-          ))}
-        </ul>
+        <ul className="history-list">{renderedHistory}</ul>
       </BottomSheet>
     </>
   );
-}
+});
 
 export default HistoryBottomSheet;
